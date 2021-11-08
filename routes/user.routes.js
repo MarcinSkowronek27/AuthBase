d232f5b8f3d9140c60bb1ed4c575d6063ea801cc
Marcin Skowronek <marcinfoto35@op.pl>
2021-11-08 13:51:28 +0100

    Remove console.log

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  console.log(req.user);
+  // console.log(req.user);
   if(!req.user) {
     res.redirect('/user/no-permission');
   } else res.render('logged', { name: req.user.displayName, image: req.user.photos[0].value});
