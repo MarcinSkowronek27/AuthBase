ea28965d3c3f2d6ce36caf00217742f422023329
Marcin Skowronek <marcinfoto35@op.pl>
2021-11-08 13:50:59 +0100

    Done task 3

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
+  console.log(req.user);
   if(!req.user) {
     res.redirect('/user/no-permission');
-  } else res.render('logged');
+  } else res.render('logged', { name: req.user.displayName, image: req.user.photos[0].value});
 });
 
 router.get('/profile', (req, res) => {
